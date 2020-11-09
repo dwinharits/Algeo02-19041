1b4b51cc3e417eb69de8777bcc1e69950b183b17
aisyahfarrasaqila <aisyahfarrasaqila@gmail.com>
2020-11-09 21:04:22 +0700

    Update program.py

--- program.py
+++ program.py
@@ -14,32 +14,39 @@ def listToString(s):
 
 def getText(link):
     source = requests.get(link).text
-
     soup = BeautifulSoup(source, 'lxml')
 
     text = []
+    separator = '\n\n'
     for body in soup.find_all('div', class_='ArticleBody-articleBody'):
         for section in body.find_all('div', class_='group'):
             for paragraph in section.find_all('p'):
                 text.append(paragraph.text)
 
+    stringText = separator.join(text)
+    return stringText
+
     return listToString(text)
 
 def getDocuments():
-    #BELUM BISA SAVE KE BEBERAPA DOCUMENT
     source = requests.get("https://www.cnbc.com/world/?region=world").text
 
     soup = BeautifulSoup(source, 'lxml')
 
-    article = soup.find('div', class_='LatestNews-newsFeed') #nanti di sini looping find_all
-    title = (article.a.text)
-    link = (article.a['href'])
-    text = getText(link)
+    i = 1
+    for article in soup.find_all('div', class_='LatestNews-newsFeed'):
+        title = (article.a.text)
+        link = (article.a['href'])
+        text = getText(link)
 
-    csv_file = open('documents.csv', 'w')
-    csv_writer = csv.writer(csv_file)
-    csv_writer.writerow([title, text])
-    csv_file.close()
+        file_name = 'document' + str(i) + '.txt'
+
+        text_file = open(file_name, 'w')
+        text_file.write(title)
+        text_file.write('\n\n')
+        text_file.write(text)
+        text_file.close()
+        i = i + 1
 
 def stopwords(sentence):
     stop_words = set(stopwords.words('english'))
