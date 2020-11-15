829912e801b6f7dfc62101cd7897ca40c83e7069
dwinharits <68527618+dwinharits@users.noreply.github.com>
2020-11-15 12:38:58 +0700

    fungsi main, belum dikomenin sama rencananya masih ada update

---
+++ mainFunction.py
@@ -0,0 +1,225 @@
+import requests
+from bs4 import BeautifulSoup
+from lxml import html
+from urllib.request import urlopen
+import nltk
+from nltk.corpus import stopwords, words
+from nltk.tokenize import word_tokenize
+from nltk.stem import PorterStemmer
+import math
+
+# KAMUS
+'''
+ps : buat stemming
+stop_words : set stop words dari nltk
+'''
+ps = PorterStemmer()
+stop_words = set(stopwords.words('english')) 
+
+# KAMUS
+'''
+getText : prosedur buat link2 gitu gatau aqila yang buat
+getDocuments : prosedur buat ngambil artikel dari web ke file txt
+DTermFrequencies : fungsi yang ngereturn array of integer TF dari suatu dokumen
+QTermFrequencies : fungsi yang ngereturn array of integer TF dari query
+STokenWord : fungsi yang ngereturn array kata-kata yang unik elemennya dan gaada stopwords
+sim : fungsi yang ngereturn cosine similarity(float)
+'''
+def getText(link):
+    source = requests.get(link).text
+    soup = BeautifulSoup(source, 'lxml')
+
+    text = []
+    separator = '\n\n'
+    for body in soup.find_all('div', class_='ArticleBody-articleBody'):
+        for section in body.find_all('div', class_='group'):
+            for paragraph in section.find_all('p'):
+                text.append(paragraph.text)
+
+    stringText = separator.join(text)
+    return stringText
+
+def getDocuments(titles,links):
+    source = requests.get("https://www.cnbc.com/world/?region=world").text
+
+    soup = BeautifulSoup(source, 'lxml')
+
+    i = 1
+    for article in soup.find_all('div', class_='LatestNews-newsFeed'):
+        title = (article.a.text)
+        titles.append(title)
+        link = (article.a['href'])
+        links.append(link)
+        text = getText(link)
+        file_name = 'document' + str(i) + '.txt'
+
+        text_file = open(file_name, 'w', encoding='utf-8', errors='ignore')
+        text_file.write(text)
+        text_file.close()
+        i = i + 1
+
+def countTerm(arr, term):
+# mengembalikan banyaknya elemen arr yang sama dengan term
+    count = 0
+    for i in arr:
+        if i==term:
+            count += 1
+    return count
+    
+def DTermFrequencies(words, namafile): #words: array of string, namafile: string.txt
+# return vektor yang koresponden dengan jumlah kemunculan kata pada dokumen berdasarkan kumpulan term pada words
+    dtf = [0 for i in words]
+    with open(namafile, 'r') as f:
+        inWords = []
+        for line in f:
+            for word in line.split():
+                inWords.append(ps.stem(word))
+        for i in range(len(words)):
+            count = 0
+            for j in inWords:
+                if(j==words[i]):
+                    count += 1
+            dtf[i] = count
+    return dtf
+
+def QTermFrequencies(words, strInput): #words: array of strings, strInput: string
+# return vektor yang koresponden dengan jumlah kemunculan kata pada query berdasarkan kumpulan term pada words
+    qtf = [0 for i in words]
+    query = []
+    for i in word_tokenize(strInput):
+        query.append(ps.stem(i))
+    for i in range(len(qtf)):
+        count = 0
+        for j in query:
+            if(j==words[i]):
+                count += 1
+        qtf[i] = count
+    return qtf
+
+def STokenWord(namafile): #namafile: string.txt
+# return kata-kata yang unik dan udah distem dan tanpa stopwords
+    words = []
+    with open(namafile, 'r') as f:
+        for line in f:
+            for word in line.split():
+                if(ps.stem(word) not in words) and (word not in stop_words):
+                    words.append(ps.stem(word))
+    return words
+
+def WordTokenize(namafile):
+    words = []
+    with open(namafile, 'r') as f:
+        for line in f:
+            for word in line.split():
+                words.append(ps.stem(word))
+    return words
+
+def getFirstSentence(namafile):
+    with open(namafile, 'r') as f:
+        line = f.readline()
+        return line
+
+
+def getJumlahKata(namafile):
+    with open(namafile,'r') as file: 
+        count = 0     
+        for line in file:        
+            for word in line.split():          
+                count += 1 
+    return count 
+
+def isDocEmpty(namafile):
+    with open(namafile,'r') as file: 
+        return len(file)==0
+
+def sim(qtf,dtf): #qtf, dtf: array of integer dari TF query dan dokumen
+    dotcount = 0
+    squareQ = 0
+    squareD = 0
+
+    for i in range (len(dtf)):
+        dotcount += qtf[i] * dtf[i]
+        squareQ += qtf[i]**2
+        squareD += dtf[i]**2
+
+    lengthQ = math.sqrt(squareQ)
+    lengthD = math.sqrt(squareD)
+
+    if(lengthQ*lengthD == 0):
+        return 0
+    else:
+        return dotcount/(lengthQ*lengthD)
+
+def getKey(item):
+    return item[0]
+
+
+####################MAIN#########################
+
+def main(query):
+    titles = []
+    links = []
+    getDocuments(titles,links)
+
+    #strInput = input("Apa yang ingin anda cari: ")
+    words = []
+    matriksTF = [[] for i in range(31)]
+    simIndex = [0 for i in range(30)]
+    wordcounts = [0 for i in range(30)]
+    firstlines = ['' for i in range(30)]
+
+    #ngebuat library term dari seluruh dokumen
+    for i in range(1,31):
+        namafile = 'document' + str(i) + '.txt'
+        setOfWords = set(words)|set(STokenWord(namafile))
+        words = list(setOfWords)
+
+    #ngeset matriksTF
+    matriksTF[0] = QTermFrequencies(words, query)
+
+    for i in range(1,31):
+        namafile = 'document' + str(i) + '.txt'
+        matriksTF[i] = DTermFrequencies(words, namafile)
+
+    #ngeset array jumlah kata
+    for i in range(30):
+        namafile = 'document' + str(i+1) + '.txt'
+        wordcounts[i] = getJumlahKata(namafile)
+
+    #ngeset array jumlah firstlines
+    for i in range(30):
+        namafile = 'document' + str(i+1) + '.txt'
+        firstlines[i] = getFirstSentence(namafile)
+
+    #ngeset array jumlah simIndex
+    for i in range(1,31):
+        simIndex[i-1] = sim(matriksTF[0], matriksTF[i])
+
+    # nanti ke-print simIndexnya, tinggal disort buat tau yang
+    # mana dokumen paling relevan, jangan lupa indeksnya 0..29
+    arrTuple = [(simIndex[i], titles[i], links[i], firstlines[i], wordcounts[i]) for i in range(30)]
+    print(sorted(arrTuple, key = getKey, reverse = True))
+
+    # buat term table
+    tQuery = query.split()
+    for term in tQuery:
+        print(term, end=': ')
+        for i in range(30):
+            namafile = 'document' + str(i+1) + '.txt'
+            print(countTerm(WordTokenize(namafile),term), end=' ')
+        print('')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
